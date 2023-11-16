42be2e018e0c4c7018db9d831d1ab5be13e03c44
Furkan Berk Canik <52695067+fberkcanik@users.noreply.github.com>
2023-11-16 15:09:40 +0300

    reactjs tutorial

---
+++ projects/erciyes-react-app/src/App.js
@@ -0,0 +1,22 @@
+import Assets from "./templates/Assets";
+import Content from "./templates/Content";
+import Footer from "./templates/Footer";
+import Header from "./templates/Header";
+import Navbar from "./templates/Navbar";
+
+ 
+
+function App() {
+  return (
+   <>
+   <Assets />
+   <Navbar />
+   <Header />
+   <Content />
+   <Footer></Footer>
+   </> 
+  );
+}
+
+export default App;
+/* Html2Jsx */
---
+++ projects/erciyes-react-app/src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Content() {
+  return (
+    <div><ul>
+        <li>İstanbul</li>
+        <li>İzmir</li>
+        </ul></div>
+  )
+}
+
+export default Content
