dbe6922540d13d69f958bcc4d481a91ee95346e3
Furkan Berk Canik <furkanberkcanik@outlook.com>
2023-11-20 15:09:13 +0300

    20kasım örnek1-2

---
+++ projects/bootcamp-ornekler/src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <ul>
+        <li><Link to="/ornek-1">Örnek - 1</Link></li>
+        <li><Link to="/ornek-2">Örnek - 2</Link></li>
+    </ul>
+  )
+}
+
+export default Header
